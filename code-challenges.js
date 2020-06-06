8f40e0d479e95d087d7409b4c53f0f648dd29bb2
jazmyn cortez <jazmyncortez@gmail.com>
2020-06-05 22:14:39 -0700

    RACHAEL ITS ACTUALLY DONE

--- code-challenges.js
+++ code-challenges.js
@@ -8,9 +8,28 @@ var people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "radio employee" }
 ]
+
+//function takes in an array
+const myFunction = (array) => {
+//create a variable instance map over the array of objects in order to get to the names
+  let sentence = array.map(person => {
+    //create a variable instance split the array to where the names are within it's own array
+    let personArray = person.name.split(" ")
+    //create a variable instance that will then be able to map over the names
+    //use charAt to acces which index of the array we want to access within the name, then use substring to tell when to stop capatilizing letters
+    let upperCase = personArray.map(name => {
+      return name.charAt(0).toUpperCase()+name.substring(1)
+      //join the strings together
+    }).join(" ")
+    //return sentence of array with string interperlation
+    return `${upperCase} is a ${person.occupation}.`
+  })
+  //join the function output together so it's not within an array rather it's a sentence
+  return sentence.join(" ")
+}
+//call my function
+// console.log(myFunction(people));
 // Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
-
-
 
 
 
@@ -21,12 +40,32 @@ var testingArray1 = [23, "Heyyyy!", 45, -9, 0, "Yo", false]
 var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
-
-
-
+//create myFunction takes in an array
+const remainderOfThree = (array) => {
+  //filter array to get onlyNums
+  let onlyNums = array.filter(value => typeof value === 'number')
+  //map over onlyNums and divide by 3 - use modulo cause we want remainders rather than answers of normal divisioins
+  return onlyNums.map(value => value%3)
+}
+  //return remainders
+// console.log(remainderOfThree(testingArray1));
+// console.log(remainderOfThree(testingArray2));
 
 // --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 
 var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
 var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
 // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+
+//create a function that has a name that makes sense and takes in two arrays
+const comboNoDups = (array1, array2) => {
+  //create a variable that will hold a method to be able to combine the two arrays as one - use .concat
+  let comboArray = array1.concat(array2)
+  //then filter through the big array
+  return comboArray.filter((value, index) => {
+    //we don't care about the index of each array rather we care about the value of each array, so by using .indexof method within filter, we'll be able to compare values within the array to return only one instance of each value
+    return comboArray.indexOf(value) === index
+  })
+}
+//call the function!
+console.log(comboNoDups(testingArray3, testingArray4))
--- interview-questions.md
+++ interview-questions.md
@@ -3,44 +3,46 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. In a model called Animal that has_many Sightings, what is the name of the foreign key? Would the foreign key be part of the Animal model or the Sightings model?
 
-  Your answer:
+  Your answer: the foreign key would be part of the sightings model, and the foreign key would be called animal_id.
 
-  Researched answer:
+  Researched answer: naming conventions for a foreign key the target table name, followed by the source table name. The table containing the foreign key is called the child table, and the table containing the candidate key is called the referenced or parent table.
 
 
 
 2. Which routes must always be passed params and why?
 
-  Your answer:
+  Your answer: the routes that are creating, updating, destroying and showing all need to have params passed through them. This is so the user knows exactly what they are accessing and are creating.
 
-  Researched answer:
+  Researched answer: Parameters are how your Rails server receives information from the user to dynamically modify the view.
 
 
 
 3. Write a rails route for a controller called "main" and a page called "game" that takes in a parameter called "guess".
 
-  Your answer:
+  Your answer:  //@main = params.require(:game).permit(:guess)
 
 
 
 4. Name three rails generator commands. What is created by each?
 
-  Your answer:
+  Your answer:rails s - starts the server
+              rails new my_app -d postgresql -T - generates the rails application
+              rails g resource model my_model - generates and database table for app
 
-  Researched answer:
+  Researched answer: it's actually - rails g resource model blankets softness:string - both the migration and model that I need to start using guitars as an ActiveRecord class. It created a controller (and a guitar views folder) so that I can start coding up some controller actions. And finally, it plugged in resourced routes for my guitars.
 
 
 
 5. Consider the Rails routes below. Describe the responsibility of each route.
 
-/users        method="GET"
+/users        method="GET" - gets all the users within this database users
 
-/users/1      method="GET"
+/users/1      method="GET" - gets the user that has the id of 1 within the database users
 
-/users/new    method="GET"
+/users/new    method="GET" - midway point of creating and destroy
 
-/users/       method="POST"
+/users/       method="POST" - creating a new object within the database
 
-/users/1      method="PUT"
+/users/1      method="PUT" - updating a specific user that has the id of 1 within the database users
 
-/users/1      method="DELETE"
+/users/1      method="DELETE" - deleting the id of 1 within the database of users
